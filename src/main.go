5fa6d7657cfaeca8cb11a9b7fc89a0cce5226e5b
gitSaein <gitSaein@gmail.com>
2021-12-31 13:54:37 +0900

    todo list complete

--- src/main.go
+++ src/main.go
@@ -6,10 +6,11 @@ import (
 	"io/ioutil"
 	"os"
 	"sort"
+	"strconv"
 	"time"
 )
 
-const FILE_PATH = "./todoList.json"
+const FILE_PATH = "./tmp/todoList.json"
 
 type Todos struct {
 	Todos []Todo `json:"todos"`
@@ -19,17 +20,13 @@ type Todo struct {
 	ID        int       `json:"id"`
 	Content   string    `json:"content"`
 	IsDone    bool      `json:"is_done"`
+	IsDeleted bool      `json:"is_deleted"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func sortByFieldName(todos Todos) {
-
-}
-
-func checkWantExit() {
-
-}
-
+// 생성
+// 2.1. a or add 입력하고, 추가할 데이터를 입력하고 엔터하면 저장되고 리스트(입력된 시간 역순)를 보여준다.
+// 2.2. !exit 입력하면, 취소되고 home으로 돌아간다.
 func createTodo(data Todos) Todos {
 	var cmdInput string
 	var isExit = false
@@ -51,22 +48,72 @@ func createTodo(data Todos) Todos {
 	return data
 }
 
+// 완료
+// 3.  c or complate 입력하면,  리스트를 불러오고, index를 입력하면 완료 표시한다.
+// 3.1. !exit 입력하면, 취소되고 home으로 돌아간다.
 func completeTodo(data Todos) Todos {
-	fmt.Println("[complate] Welcome to TODO list ================================")
+	var cmdInput string
+	var isExit = false
+
+	for !isExit {
+
+		getTodoList(data, "complete")
+		fmt.Print("complate item > ")
+		fmt.Scanln(&cmdInput)
+
+		if cmdInput == "!exit" {
+			return data
+		}
+		idx, err := strconv.Atoi(cmdInput)
+		if err != nil || idx > len(data.Todos) {
+			continue
+		}
+
+		data.Todos[len(data.Todos)-idx].IsDone = !data.Todos[len(data.Todos)-idx].IsDone
+
+	}
 
 	return data
-
 }
 
+// 삭제
+// 4.  d or delete 입력하면,  리스트를 불러오고, index를 입력하면 완료 표시한다.
+// 4.1. !exit 입력하면, 취소되고 home으로 돌아간다.
 func deleteTodo(data Todos) Todos {
-	fmt.Println("[delete] Welcome to TODO list ================================")
+	var cmdInput string
+	var isExit = false
+
+	for !isExit {
+
+		getTodoList(data, "delete")
+		fmt.Print("delete item > ")
+		fmt.Scanln(&cmdInput)
+
+		if cmdInput == "!exit" {
+			return data
+		}
+		cmdInputToInt, err := strconv.Atoi(cmdInput)
+		if err != nil || cmdInputToInt > len(data.Todos) {
+			continue
+		}
+		// idx := len(data.Todos) - cmdInputToInt
+		// data.Todos = append(data.Todos[:idx], data.Todos[idx+1:]...)
+		for i, todo := range data.Todos {
+			if todo.ID == cmdInputToInt {
+				data.Todos[i].IsDeleted = true
+			}
+		}
+
+	}
 
 	return data
 
 }
 
-func getTodoList(data Todos) {
-	fmt.Println("[home] Welcome to TODO list ================================")
+// 리스트 불러오기
+func getTodoList(data Todos, pagename string) {
+	fmt.Printf("[%s] Welcome to TODO list ================================", pagename)
+	fmt.Println()
 
 	lenOfData := len(data.Todos)
 
@@ -79,6 +126,11 @@ func getTodoList(data Todos) {
 		})
 
 		for i := 0; i < lenOfData; i++ {
+
+			if data.Todos[i].IsDeleted {
+				continue
+			}
+
 			if data.Todos[i].IsDone {
 				fmt.Printf("%d. (%s) ", data.Todos[i].ID, "V")
 			} else {
@@ -91,7 +143,8 @@ func getTodoList(data Todos) {
 	fmt.Println("===================================================")
 }
 
-func readTodoListFile() Todos {
+// 리스트 불러오기
+func readFile() Todos {
 
 	data := Todos{}
 
@@ -109,12 +162,12 @@ func readTodoListFile() Todos {
 	}
 
 	json.Unmarshal([]byte(byteValueFile), &data)
-	fmt.Println(data)
 
 	return data
 
 }
 
+// 파일 읽어오기
 func writeTodoListFile(todos Todos) (Todos, error) {
 	jsonTodos, err := json.Marshal(todos)
 	if err != nil {
@@ -125,6 +178,7 @@ func writeTodoListFile(todos Todos) (Todos, error) {
 	return todos, err
 }
 
+// 명령어 확인하기
 func checkCmdInput(input string, todos Todos) {
 	switch {
 	case input == "a" || input == "add":
@@ -150,17 +204,13 @@ func main() {
 	// 1. todo list가 있는지 없는지 확인해서 있으면 리스트 불러오기
 	for isEnd {
 		var cmdInput string
-		data := readTodoListFile()
-		getTodoList(data)
-		// 2.1. a or add 입력하고, 추가할 데이터를 입력하고 엔터하면 저장되고 리스트(입력된 시간 역순)를 보여준다.
+		data := readFile()
+		getTodoList(data, "home")
 		fmt.Print("Enter> ")
 		fmt.Scanln(&cmdInput)
+
 		checkCmdInput(cmdInput, data)
-		// 2.2. !exit 입력하면, 취소되고 home으로 돌아간다.
-		// 3.  c or complate 입력하면,  리스트를 불러오고, index를 입력하면 완료 표시한다.
-		// 3.1. !exit 입력하면, 취소되고 home으로 돌아간다.
-		// 4.  d or delete 입력하면,  리스트를 불러오고, index를 입력하면 완료 표시한다.
-		// 4.1. !exit 입력하면, 취소되고 home으로 돌아간다.
+
 	}
 
 }
